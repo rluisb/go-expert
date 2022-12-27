15f154677feabf0c067ed8898eef1b169f266a5d
Ricardo Conceição <ricardo220893@gmail.com>
2022-12-27 18:40:29 -0300

    add context classes;

--- 14-context/2-http/server/main.go
+++ 14-context/2-http/server/main.go
@@ -1,1 +1,30 @@
-package __http
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+	log.Println("Request iniciada")
+	defer log.Println("Request finalizada")
+
+	select {
+	case <-time.After(5 * time.Second):
+		message := "Request processada com sucesso"
+		log.Println(message)
+		w.Write([]byte(message))
+	case <-ctx.Done():
+		message := "Request cancelada pelo cliente"
+		log.Println(message)
+		http.Error(w, message, http.StatusRequestTimeout)
+	}
+
+}
