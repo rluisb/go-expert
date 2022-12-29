204eb4762e3ebdeb1517c1d5889857ceabf05b9f
Ricardo Conceição <ricardo220893@gmail.com>
2022-12-29 14:38:30 -0300

    start db classes

---
+++ 15-database/1-without-framweork/go.mod
@@ -0,0 +1,8 @@
+module github.com/rluisb/curso-go/15-database/1-without-framework
+
+go 1.19
+
+require (
+	github.com/go-sql-driver/mysql v1.7.0
+	github.com/google/uuid v1.3.0
+)
---
+++ 15-database/1-without-framweork/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:3306)/goexpert")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+}
