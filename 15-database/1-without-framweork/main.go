2a07b6ef915a080c36fd6a1df1c68b08bfe07c19
Ricardo Conceição <ricardo220893@gmail.com>
2022-12-29 19:16:52 -0300

    implement update product

--- 15-database/1-without-framweork/main.go
+++ 15-database/1-without-framweork/main.go
@@ -31,6 +31,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+	product.Price = 100.00
+	err = updateProduct(db, product)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func insertProduct(db *sql.DB, product *Product) error {
@@ -40,6 +45,19 @@ func insertProduct(db *sql.DB, product *Product) error {
 	}
 	defer stmt.Close()
 	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("UPDATE products SET name = ?, price = ? WHERE id = ?")
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
 	if err != nil {
 		return err
 	}
